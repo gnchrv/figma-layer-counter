673ab280950cd184efe53557a47ac6f35668f7b8
Misha Goncharov <forsomeletters@gmail.com>
2020-12-16 01:52:08 +0300

    Actually wrote the plugin

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
-if (figma.command == 'first-command')
-    console.log('The first command was executed')
+// Import the library
+import { Counter, countLayers } from './lib'
 
-if (figma.command == 'second-command')
-    console.log('The second command was executed')
+// Get the layers selected
+let { selection } = figma.currentPage
 
+// If nothing is selected consider all the nodes presented on the page to be selected
+if (!selection.length) selection = figma.currentPage.children
+
+// Count selected layers
+const result: Counter = countLayers(selection as SceneNode[])
+
+// Log the full result to a console
+console.log(result)
+
+// Compose the result message from the data received
+const { total, hidden, topLevel } = result
+const msg = `${total} elements in total, ${topLevel} top-level (${hidden} hidden)`
+
+// Close the plugin and show the result
+figma.closePlugin(msg)
